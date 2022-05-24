e1634bbabb1d1baef853680580d32f5789ab02b6
Felipe <fnasccunha@gmail.com>
2022-05-24 11:54:21 -0300

    fix route redirect

--- src/pages/components/Header/Header.tsx
+++ src/pages/components/Header/Header.tsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <div className={scss.container}>
       <div className={scss.logoWrapper}>
-        <InternalLink to="/" className={scss.logoLink}>
+        <InternalLink to="/ViniciusBSI" className={scss.logoLink}>
           <p className={scss.enterpriseName}>
             Consultorio<span className={scss.me}>.me</span>
           </p>
@@ -15,7 +15,7 @@ function Header() {
       <div className={scss.contentWrapper}>
         <ul className={scss.itemsWrapper}>
           <li className={scss.item}>
-            <a className={scss.link} href="/lista-espera">
+            <a className={scss.link} href="/ViniciusBSI/lista-espera">
               Visualizar lista de espera
             </a>
           </li>
